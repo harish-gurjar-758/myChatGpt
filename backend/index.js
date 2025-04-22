ab6854edab2219954e0875dca324bf06918bad72
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-04-22 11:05:47 +0530

    create module of users

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,24 @@ import userRoutes from './src/routes/user.routes.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+const connectToMongoDB = async () => {
+  const connection = await mongoose.connect(process.env.MONGODB_URL).catch(err => err);
 
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => console.log("✅ Connected to MongoDB Atlas"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  if (connection instanceof mongoose.Mongoose) {
+    console.log("✅ Connected to MongoDB Atlas");
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+    app.get('/', (req, res) => {
+      res.send('API is running...');
+    });
 
+  } else {
+    console.error("❌ MongoDB connection error:", connection);
+  }
+};
+connectToMongoDB();
 app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -4,21 +4,15 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
-    trim: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
-    lowercase: true
   },
   password: {
     type: String,
     required: true
   },
-}, {
-  timestamps: true // Automatically adds createdAt & updatedAt
 });
 
 const User = mongoose.model('User', userSchema);
